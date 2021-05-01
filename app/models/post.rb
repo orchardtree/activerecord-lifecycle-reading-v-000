79f8bf73fefcd41aac70e648ec4dcb69527c05b4
Jesse Wenger <wengerj@gmail.com>
2021-05-01 20:00:08 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,11 @@ class Post < ActiveRecord::Base
 
   private
 
+  def email_author_about_post
+  # Not implemented.
+  # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
+
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
